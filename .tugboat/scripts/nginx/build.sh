0726d74c7b280233994c74127373aa37f16be96f
James Bain <jamescbain@gmail.com>
2023-07-04 11:52:46 -0600

    uncomment build step in nginx

--- .tugboat/scripts/nginx/build.sh
+++ .tugboat/scripts/nginx/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt install -y nodejs
+npm install -g wscat
+
 cd $TUGBOAT_ROOT/revproxy
 cp ./tugboat.conf /etc/nginx/conf.d/default.conf
-/usr/sbin/nginx -s reload
+
+
