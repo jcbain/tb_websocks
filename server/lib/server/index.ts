f793a5ccb73b227b3289f768313b637ac461dd91
James Bain <jamescbain@gmail.com>
2024-09-11 15:48:14 -0600

    increase timeout

--- server/lib/server/index.ts
+++ server/lib/server/index.ts
@@ -8,7 +8,7 @@ export const start = async function (opts: FastifyPluginOptions) {
   server.register(fastifyWebsocket);
 
   server.get("/long", async (req, reply) => {
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    await new Promise((resolve) => setTimeout(resolve, 100000));
     return "wowee";
   });
 
